73a774fdf7dc89cd607a87fec680d17aeecd55cd
spog <spog>
2006-02-21 15:15:35 +0000

    fixed gcc compile errors
    git-svn-id: svn://svn.icculus.org/gtkradiant/GtkRadiant/trunk@18 8a3a26a2-13c4-0310-b231-cf6edde360e5

--- libs/stream/textstream.h
+++ libs/stream/textstream.h
@@ -32,6 +32,7 @@ Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 #include <cmath>
 #include <stdio.h>
 #include <string.h>
+#include <algorithm>
 
 #include "generic/arrayrange.h"
 
