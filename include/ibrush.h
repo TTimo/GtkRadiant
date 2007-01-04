e1a4c59897d2a25226e0e9d7cde4f000326f744f
Forest Hale <lordhavoc>
2007-01-04 16:10:21 +0000

    added Node_getBrush function as requested by mattn (UFO:AI team)
    git-svn-id: svn://svn.icculus.org/gtkradiant/GtkRadiant/trunk@129 8a3a26a2-13c4-0310-b231-cf6edde360e5

--- include/ibrush.h
+++ include/ibrush.h
@@ -124,4 +124,9 @@ inline BrushCreator& GlobalBrushCreator()
   return GlobalBrushModule::getTable();
 }
 
+inline Brush* Node_getBrush(scene::Node& node)
+{
+  return NodeTypeCast<Brush>::cast(node);
+}
+
 #endif
