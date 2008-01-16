dd22859189e2581149d4d7ef05211bbdfc5b038a
Forest Hale <lordhavoc>
2008-01-16 05:30:49 +0000

    patch from Ciaran Gillespie to fix a g++ 4 compile error regarding functions prototyped with (GLvoid)
    git-svn-id: svn://svn.icculus.org/gtkradiant/GtkRadiant/trunk@194 8a3a26a2-13c4-0310-b231-cf6edde360e5

--- include/igl.h
+++ include/igl.h
@@ -2645,7 +2645,7 @@ struct OpenGLBinding
   GLhandleARB (QGL_DLLEXPORT *m_glCreateShaderObjectARB)(GLenum shaderType);
   void (QGL_DLLEXPORT *m_glShaderSourceARB)(GLhandleARB shaderObj, GLsizei count, const GLcharARB **string, const GLint *length);
   void (QGL_DLLEXPORT *m_glCompileShaderARB)(GLhandleARB shaderObj);
-  GLhandleARB (QGL_DLLEXPORT *m_glCreateProgramObjectARB)(GLvoid);
+  GLhandleARB (QGL_DLLEXPORT *m_glCreateProgramObjectARB)(void);
   void (QGL_DLLEXPORT *m_glAttachObjectARB)(GLhandleARB containerObj, GLhandleARB obj);
   void (QGL_DLLEXPORT *m_glLinkProgramARB)(GLhandleARB programObj);
   void (QGL_DLLEXPORT *m_glUseProgramObjectARB)(GLhandleARB programObj);
