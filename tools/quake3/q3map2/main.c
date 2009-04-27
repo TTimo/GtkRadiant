4f58d60bc37d3ec5d8097d27501b7b7a0b579943
divverent <divverent@61c419a2-8eb2-4b30-bcec-8cead039b335>
2009-04-27 09:20:03 +0000

    remove two debug prints
    git-svn-id: svn://svn.icculus.org/netradiant/trunk@341 61c419a2-8eb2-4b30-bcec-8cead039b335

--- tools/quake3/q3map2/main.c
+++ tools/quake3/q3map2/main.c
@@ -498,7 +498,6 @@ void MiniMapMakeSampleOffsets()
 					minimap.sample_offsets[2*j+0] = rx;
 					minimap.sample_offsets[2*j+1] = ry;
 					i = -1;
-					Sys_Printf("%f\n", val);
 				}
 				else
 				{
@@ -513,7 +512,6 @@ void MiniMapMakeSampleOffsets()
 					minimap.sample_offsets[2*k+0] = rx;
 					minimap.sample_offsets[2*k+1] = ry;
 					i = -1;
-					Sys_Printf("%f\n", val);
 				}
 				else
 				{
