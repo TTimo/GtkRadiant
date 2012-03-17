515673c08f8718a237e90c2130a1f5294f966d6a
Timothee TTimo Besset <ttimo@idsoftware.com>
2012-03-17 14:07:19 -0500

    trash a bunch of outdated and confusing things

--- compile_ibuild_win32.bat
+++
@@ -1,5 +0,0 @@
-rem silly build script for compiling with MSVC using the IncrediBuild console
-rem can't figure how to use the buildbot ShellCommand to pass the correct CFG= line
-rem because of the configuration name with a | in it (Release|Win32)
-
-buildconsole.exe radiant.sln /BUILD /CFG="Release|Win32"
--- run_python.bat
+++
@@ -1,9 +0,0 @@
-python.exe -V
-if errorlevel=1 echo please install python and add python.exe to the path (http://www.python.org)
-if errorlevel=1 goto end
-
-echo python.exe %1 %2 %3 %4 %5 %6 %7 %8 %9
-rem FIXME: for some wacked reason, output of the python script doesn't get back to VC window ..
-python.exe %1 %2 %3 %4 %5 %6 %7 %8 %9
-
-:end
