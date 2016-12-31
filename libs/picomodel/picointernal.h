b529282159f1ed1f752279d5873e7f9a1615b354
Jay Dolan <jay@jaydolan.com>
2016-12-31 15:44:18 -0500

    Fix compilation on OS X. How is this working elsewhere?

--- libs/picomodel/picointernal.h
+++ libs/picomodel/picointernal.h
@@ -132,7 +132,7 @@ void            _pico_printf( int level, const char *format, ... );
 char            *_pico_stristr( char *str, const char *substr );
 void            _pico_unixify( char *path );
 int             _pico_nofname( const char *path, char *dest, int destSize );
-char            *_pico_nopath( const char *path );
+const char      *_pico_nopath( const char *path );
 char            *_pico_setfext( char *path, const char *ext );
 int             _pico_getline( char *buf, int bufsize, char *dest, int destsize );
 char            *_pico_strlwr( char *str );
