da38f7754cb791b81428fcc7da39ace16d02cfe5
Thomas Debesse <dev@illwieckz.net>
2017-08-06 02:55:32 +0200

    [q3map2] reduce diff noise with netradiant tree

--- tools/quake3/q3map2/q3map2.h
+++ tools/quake3/q3map2/q3map2.h
@@ -1886,7 +1886,7 @@ Q_EXTERN game_t games[]
 	,
 								#include "game_tremulous.h" /*LinuxManMikeC: must be after game_quake3.h, depends on #define's set in it */
 	,
-								#include "game_unvanquished.h" /*LinuxManMikeC: must be after game_quake3.h, depends on #define's set in it */
+								#include "game_unvanquished.h" /* must be after game_quake3.h as they share defines! */
 	,
 								#include "game_tenebrae.h"
 	,
