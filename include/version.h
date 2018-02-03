6f5e9a39b4666d44d5e2ac560807136e782f77b8
Timothee Besset <ttimo@ttimo.net>
2018-02-03 17:48:17 -0600

    bump version

--- include/version.h
+++ include/version.h
@@ -1,4 +1,4 @@
 // generated header, see makeversion.py
-#define RADIANT_VERSION "1.6.5"
-#define RADIANT_MINOR_VERSION "5"
+#define RADIANT_VERSION "1.6.6"
+#define RADIANT_MINOR_VERSION "6"
 #define RADIANT_MAJOR_VERSION "6"
